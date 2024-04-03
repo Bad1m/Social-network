4912300a684dcfcd33646646e34cefc7d199c204
0102vadik <milko.vadimka@mail.ru>
2024-04-03 22:05:24 +0300

    add music app

--- src/navigations/MyStack.js
+++ src/navigations/MyStack.js
@@ -1,28 +1,30 @@
 // MyStack.js
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Posts from "../screens/Posts";
 import Detail from "../screens/Detail";
 import ContactsScreen from "../screens/ContactsScreen";
+import MusicPlayerScreen from "../screens/MusicPlayerScreen";
 
 const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
-  return (
-    <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Detail" component={Detail} />
-          <Stack.Screen name="Posts" component={Posts} />
-          <Stack.Screen name="ContactsScreen" component={ContactsScreen} />
-        </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{headerShown: false}}
+            >
+                <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Detail" component={Detail}/>
+                <Stack.Screen name="Posts" component={Posts}/>
+                <Stack.Screen name="ContactsScreen" component={ContactsScreen}/>
+                <Stack.Screen name="MusicPlayerScreen" component={MusicPlayerScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
-export { MyStack };
+export {MyStack};
